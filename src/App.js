f81fa736f7eed7584ba632ebe610c36474807553
AndyMalela <ifal.n.malela@gmail.com>
2025-07-14 01:22:27 +0900

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
