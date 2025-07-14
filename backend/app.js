b6074432451ca4c79a0289ca496a1fd1f99aa409
AndyMalela <ifal.n.malela@gmail.com>
2025-07-14 14:00:10 +0900

    Serve React static files from backend using Express

--- backend/app.js
+++ backend/app.js
@@ -16,4 +16,13 @@ app.use("/api/courses", courseRoutes);
 app.use('/api/professors', professorRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/ratings", ratingRoutes);
+
+// Serve static files from the React app
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// The "catchall" handler: for any request that doesn't match an API route, send back React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 module.exports = app;
